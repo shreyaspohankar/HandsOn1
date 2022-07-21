31905c0d8879d8418356f276d345a6497b359ae8
Shreyas <shreyas.pohankar@gamil.com>
2022-07-21 12:59:24 +0530

    Adding the project

---
+++ src/main/java/com/wipro/HandsOn1/App.java
@@ -0,0 +1,13 @@
+package com.wipro.HandsOn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Wipro" );
+    }
+}
